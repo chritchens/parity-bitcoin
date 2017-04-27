4263a3717076a78ff2fb5cabd201103b08220b6b
Travis CI User <travis@example.org>
2017-04-27 15:55:50 +0000

    Update documentation

---
+++ implementors/bitcrypto/trait.Digest.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["bitcrypto"] = ["impl <a class='trait' href='bitcrypto/trait.Digest.html' title='bitcrypto::Digest'>Digest</a> for <a class='struct' href='bitcrypto/struct.DHash160.html' title='bitcrypto::DHash160'>DHash160</a>","impl <a class='trait' href='bitcrypto/trait.Digest.html' title='bitcrypto::Digest'>Digest</a> for <a class='struct' href='bitcrypto/struct.DHash256.html' title='bitcrypto::DHash256'>DHash256</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/rand/trait.Rng.js
@@ -0,0 +1,42 @@
+(function() {var implementors = {};
+implementors["arrayvec"] = [];
+implementors["bitcrypto"] = ["impl <a class='trait' href='rand/trait.Rng.html' title='rand::Rng'>Rng</a> for <a class='struct' href='crypto/fortuna/struct.Fortuna.html' title='crypto::fortuna::Fortuna'>Fortuna</a>",];
+implementors["bytes"] = [];
+implementors["chain"] = [];
+implementors["clap"] = [];
+implementors["crypto"] = ["impl <a class='trait' href='rand/trait.Rng.html' title='rand::Rng'>Rng</a> for <a class='struct' href='crypto/fortuna/struct.Fortuna.html' title='crypto::fortuna::Fortuna'>Fortuna</a>",];
+implementors["db"] = [];
+implementors["hyper"] = [];
+implementors["import"] = ["impl <a class='trait' href='rand/trait.Rng.html' title='rand::Rng'>Rng</a> for <a class='struct' href='crypto/fortuna/struct.Fortuna.html' title='crypto::fortuna::Fortuna'>Fortuna</a>",];
+implementors["jsonrpc_core"] = [];
+implementors["jsonrpc_http_server"] = [];
+implementors["jsonrpc_macros"] = [];
+implementors["jsonrpc_pubsub"] = [];
+implementors["keys"] = [];
+implementors["libc"] = [];
+implementors["message"] = [];
+implementors["mio"] = [];
+implementors["network"] = ["impl <a class='trait' href='rand/trait.Rng.html' title='rand::Rng'>Rng</a> for <a class='struct' href='crypto/fortuna/struct.Fortuna.html' title='crypto::fortuna::Fortuna'>Fortuna</a>",];
+implementors["owning_ref"] = [];
+implementors["p2p"] = [];
+implementors["parking_lot"] = [];
+implementors["parking_lot_core"] = [];
+implementors["primitives"] = [];
+implementors["rand"] = [];
+implementors["regex_syntax"] = [];
+implementors["rocksdb"] = [];
+implementors["rpc"] = [];
+implementors["script"] = [];
+implementors["serde"] = [];
+implementors["serialization"] = [];
+implementors["syn"] = [];
+implementors["tokio_core"] = [];
+implementors["verification"] = ["impl <a class='trait' href='rand/trait.Rng.html' title='rand::Rng'>Rng</a> for <a class='struct' href='crypto/fortuna/struct.Fortuna.html' title='crypto::fortuna::Fortuna'>Fortuna</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
